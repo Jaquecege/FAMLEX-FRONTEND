d7eead5e29fab633cf8286fea286976061be00b0
Jaquecege <yolala1712@gmail.com>
2025-06-10 19:55:17 -0600

    Corrijo envío de datos para login/register como FormData

--- src/api/famlexApi.js
+++ src/api/famlexApi.js
@@ -1,13 +1,21 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
 // Registro
-export const registrarUsuario = (email, password) =>
-  API.post("/register", { email, password });
+export const registrarUsuario = (email, password) => {
+  const form = new FormData();
+  form.append("username", email);
+  form.append("password", password);
+  return API.post("/register", form);
+};
 
 // Login
-export const iniciarSesion = (email, password) =>
-  API.post("/login", { email, password });
+export const iniciarSesion = (email, password) => {
+  const form = new FormData();
+  form.append("username", email);
+  form.append("password", password);
+  return API.post("/login", form);
+};
